7f5b8161873d1043090ab63d6dc3fb3e9eea95d6
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-03-12 22:03:36 +0100

    Builded structure of project

---
+++ src/main/java/pl/coderslab/Entity/UserDao.java
@@ -0,0 +1,116 @@
+package pl.coderslab.Entity;
+
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.DbUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDao {
+    private static final String CREATE = "INSERT INTO users(email, username, password) VALUES(?,?,?);";
+    private static final String READ = "SELECT * FROM users WHERE id = ?;";
+    private static final String UPDATE = "UPDATE users SET email = ?,username = ?, password = ? WHERE ID = ?;";
+    private static final String FIND_ALL = "SELECT * FROM users;";
+    private static final String DELETE = "DELETE FROM users WHERE id = ?;";
+
+
+    private static String hashPassword(String password) {
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+        return hashedPassword;
+    }
+
+    public User create(User user) {
+        try (Connection conn = DbUtil.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(CREATE, PreparedStatement.RETURN_GENERATED_KEYS)) {
+                ps.setString(1, user.getEmail());
+                ps.setString(2, user.getUsername());
+                ps.setString(3, hashPassword(user.getPassword()));
+                ps.executeUpdate();
+                try (ResultSet rs = ps.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        user.setId(rs.getInt(1));
+                    }
+                }
+                return user;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public User read(int id) {
+        User user = new User();
+        try (Connection conn = DbUtil.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(READ)) {
+                ps.setInt(1, id);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (!rs.next()) {
+                        System.out.println("User with id " + id + " not found");
+                        return null;
+                    }
+                    user.setId(rs.getInt(1));
+                    user.setEmail(rs.getString("email"));
+                    user.setUsername(rs.getString("username"));
+                    user.setPassword(rs.getString("password"));
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return user;
+    }
+
+
+    public void update(User user) {
+        try (Connection conn = DbUtil.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(UPDATE)) {
+                ps.setString(1, user.getEmail());
+                ps.setString(2, user.getUsername());
+                ps.setString(3, hashPassword(user.getPassword()));
+                ps.setInt(4, user.getId());
+                ps.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void delete(int id) {
+        try (Connection conn = DbUtil.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(DELETE)) {
+                ps.setInt(1, id);
+                ps.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+
+        }
+    }
+
+    public List<User> findAll() {
+        List<User> users = new ArrayList<>();
+
+        try (Connection conn = DbUtil.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(FIND_ALL)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        users.add(new User(rs.getInt("id"),
+                                rs.getString("email"),
+                                rs.getString("username"),
+                                rs.getString("password")
+                        ));
+                    }
+                }
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return users;
+    }
+}
---
+++ src/main/webapp/footer.jsp
@@ -0,0 +1,24 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2025</span>
+        </div>
+    </div>
+</footer>
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<script src="js/sb-admin-2.min.js"></script>
+
+<script src="vendor/chart.js/Chart.min.js"></script>
+<script src="js/demo/chart-area-demo.js"></script>
+<script src="js/demo/chart-pie-demo.js"></script>
+
+</body>
+</html>
+
